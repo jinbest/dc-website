b7821831caeeb11b4e15b8497544f1eb4940eb77
jinbest <jin.bestvictoria718@gmail.com>
2021-06-11 19:46:59 +0800

    privacy modal structure completed

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,20 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "prettier",
   ],
   rules: {
     "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/require-await": "off",
-    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
     "no-useless-escape": "off",
   },
   parserOptions: {
--- components/PrivacyModal.tsx
+++ components/PrivacyModal.tsx
@@ -49,7 +49,96 @@ const PrivacyModal = ({ openModal, handleModal }: Props) => {
                 )
               })}
               <p className="modal-content">{privacyData.content}</p>
-
+              <ol className="modal-data-container">
+                {_.sortBy(privacyData.data, (o) => o.order).map((item: any, index: number) => {
+                  return (
+                    <div key={index}>
+                      {item.visible ? (
+                        <div>
+                          <li
+                            className="modal-conten"
+                            style={{ textTransform: "uppercase", padding: "7px 0" }}
+                          >
+                            <span>{item.title}</span>
+                          </li>
+                          <div>
+                            {item.children.map((it: any, idx: number) => {
+                              return (
+                                <div key={`${index}-${idx}`}>
+                                  <p className="modal-content">
+                                    <span className="bold" style={{ marginRight: "15px" }}>{`${
+                                      index + 1
+                                    }.${idx + 1}`}</span>
+                                    <span className="bold">{it.title.bold}</span>&nbsp;
+                                    <span
+                                      style={{ fontWeight: item.contentBold ? "bold" : "inherit" }}
+                                    >
+                                      {it.title.thin}
+                                    </span>
+                                  </p>
+                                  {it.child.length ? (
+                                    <ul>
+                                      {it.child.map((itChild: any, idxChild: number) => {
+                                        return (
+                                          <div key={`${index}-${idx}-${idxChild}`}>
+                                            <li
+                                              style={{ listStyle: "lower-alpha" }}
+                                              className="modal-content"
+                                            >
+                                              <div style={{ marginLeft: "10px" }}>
+                                                <span
+                                                  className="modal-content"
+                                                  style={{
+                                                    fontWeight: item.contentBold
+                                                      ? "bold"
+                                                      : "inherit",
+                                                  }}
+                                                >
+                                                  {itChild.title}
+                                                </span>
+                                                {itChild.child.length ? (
+                                                  <ul>
+                                                    {itChild.child.map((itC: any, idxC: number) => {
+                                                      return (
+                                                        <li
+                                                          style={{
+                                                            listStyle: "lower-roman",
+                                                            fontWeight: item.contentBold
+                                                              ? "bold"
+                                                              : "inherit",
+                                                          }}
+                                                          className="modal-content"
+                                                          key={`${index}-${idx}-${idxChild}-${idxC}`}
+                                                        >
+                                                          {itC}
+                                                        </li>
+                                                      )
+                                                    })}
+                                                  </ul>
+                                                ) : (
+                                                  <></>
+                                                )}
+                                              </div>
+                                            </li>
+                                          </div>
+                                        )
+                                      })}
+                                    </ul>
+                                  ) : (
+                                    <></>
+                                  )}
+                                </div>
+                              )
+                            })}
+                          </div>
+                        </div>
+                      ) : (
+                        <></>
+                      )}
+                    </div>
+                  )
+                })}
+              </ol>
               <p className="modal-content bold">{privacyData.footer.name}</p>
               <p className="modal-content bold">{`Mailing Address: ${privacyData.footer.address}`}</p>
               <p className="modal-content bold">
